9831ad95b2e2b6ffd03f7ef61a3580b8cee27ead
RHplusSEUNG <ctacm200@naver.com>
2023-05-15 15:19:18 +0900

    230515

--- Assets/Scripts/SceneLoad.cs
+++ Assets/Scripts/SceneLoad.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class SceneLoad : MonoBehaviour
 {
     Button startButton;
-
+    [SerializeField] TMP_InputField startX, startY, endX, endY;
+    
     public void Load()
     {
         SceneManager.LoadScene("DrawingScene");
+    }
+
+    public void PointRelay()
+    {
+
     }
 }
